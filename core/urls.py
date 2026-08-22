bf9ac28940e267287ea419c9b77e69d1ef740ba0
jefferson guedes <jeffersonguedes0211@gmail.com>
2026-08-22 11:18:43 -0300

    Feat: Primeiro commit

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.views.static import serve
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-integration/', admin.site.urls),
     path('api/integration/', include('integration.urls')),
 
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
